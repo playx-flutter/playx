79b441470a42b9b0721cfe4d6f475d6ebfa830fe
Basem Osama <engbasemosama@gmail.com>
2023-11-28 15:49:47 +0200

    update version to 1.0.2

--- lib/playx.dart
+++ lib/playx.dart
@@ -1,22 +1,34 @@
 export 'package:async/async.dart' hide Result;
 export 'package:fimber/fimber.dart';
+export 'package:flutter_animate/flutter_animate.dart'
+    hide NumDurationExtensions;
 export 'package:flutter_readable/flutter_readable.dart';
+export 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 export 'package:playx/src/config/playx_app_config.dart';
-export 'package:playx/src/utils/is_equal_validation.dart';
 export 'package:playx/src/playx.dart';
-export 'package:playx_theme/playx_theme.dart' hide FormData,MultipartFile,Response;
+export 'package:playx/src/utils/is_equal_validation.dart';
+export 'package:playx/src/widgets/playx_material_app.dart';
+export 'package:playx/src/widgets/playx_platform_app.dart';
+export 'package:playx_localization/playx_localization.dart'
+    hide FormData, Response, MultipartFile;
+export 'package:playx_network/playx_network.dart';
+export 'package:playx_theme/playx_theme.dart'
+    hide FormData, MultipartFile, Response;
 export 'package:playx_widget/playx_widget.dart';
 export 'package:queen_validators/queen_validators.dart';
 export 'package:sentry_flutter/sentry_flutter.dart'
-    hide HttpSanitizer, UrlDetails, HttpHeaderUtils, SentryTraceOrigins,getUtcDateTime,formatDateAsIso8601WithMillisPrecision ,jsonSerializationFallback;
-export 'package:playx_network/playx_network.dart';
-export 'package:playx/src/widgets/playx_material_app.dart';
-export 'package:playx/src/widgets/playx_platform_app.dart';
-export 'package:playx_localization/playx_localization.dart' hide FormData, Response, MultipartFile;
-export 'package:flutter_animate/flutter_animate.dart' hide NumDurationExtensions;
+    hide
+        HttpSanitizer,
+        UrlDetails,
+        HttpHeaderUtils,
+        SentryTraceOrigins,
+        getUtcDateTime,
+        formatDateAsIso8601WithMillisPrecision,
+        jsonSerializationFallback;
+
+export 'src/connection_status/connection_status_controller.dart';
 export 'src/widgets/models/playx_app_settings.dart';
 export 'src/widgets/models/playx_navigation_settings.dart';
 export 'src/widgets/models/playx_platform_settings.dart';
 export 'src/widgets/models/playx_screen_settings.dart';
 export 'src/widgets/models/playx_theme_settings.dart';
-export 'src/connection_status/connection_status_controller.dart';
